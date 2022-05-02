299c861ede44548b97b279423d7c4a380ad5e15d
clackgot <clackgot@gmail.com>
2022-05-03 02:44:31 +0300

    Теперь в таблицу заноситься ссылки, а не ID

--- ParserYoula/Program.cs
+++ ParserYoula/Program.cs
@@ -65,17 +65,25 @@ namespace ParserYoula
             {
                 products.Add(product);
             }
-            Save(products);
+            if(!string.IsNullOrEmpty(result.subcategorySlug))
+            {
+                Save(products, result.subcategorySlug);
+            }
+            else
+            {
+                Save(products, result.categorySlug);
+            }
+
         }
 
 
-        private static void Save(List<Product> products)
+        private static void Save(List<Product> products, string listName)
         {
             var package = new ExcelPackage();
 
-            var sheet = package.Workbook.Worksheets.Add("Result");
-            sheet.Cells[1, 1].Value = "ID объявление";
-            sheet.Cells[1, 2].Value = "ID владельца";
+            var sheet = package.Workbook.Worksheets.Add(listName);
+            sheet.Cells[1, 1].Value = "Объявление";
+            sheet.Cells[1, 2].Value = "Владелец";
             sheet.Cells[1, 3].Value = "Описание";
             sheet.Cells[1, 4].Value = "Отзывов";
             sheet.Cells[1, 5].Value = "Название";
@@ -83,33 +91,18 @@ namespace ParserYoula
             int row = 2;
             int col = 1;
 
-            //List<Product> products = new List<Product>();
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    products.Add(new Product()
-            //    {
-            //        Id = $"Id{i}",
-            //        OwnerId = $"OwnerId{i}",
-            //        Description = $"Description{i}",
-            //        MarksCount = i,
-            //        Name = $"Name{i}",
-            //        Price = i * 100
-            //    });
-            //}
-
-
             foreach (var product in products)
             {
-                sheet.Cells[row, col].Value = product.Id;
-                sheet.Cells[row, col + 1].Value = product.OwnerId;
-                sheet.Cells[row, col + 2].Value = product.Description;
+                sheet.Cells[row, col].Value = $"https://youla.ru/p{product.Id}";
+                sheet.Cells[row, col + 1].Value = $"https://youla.ru/user/{product.OwnerId}";
+                sheet.Cells[row, col + 2].Value = product.Description.Trim();
                 sheet.Cells[row, col + 3].Value = product.MarksCount;
                 sheet.Cells[row, col + 4].Value = product.Name;
                 sheet.Cells[row, col + 5].Value = product.Price;
                 row++;
             }
 
-            sheet.Protection.IsProtected = true;
+            sheet.Protection.IsProtected = false;
             var excel = package.GetAsByteArray();
             File.WriteAllBytes("result.xlsx", excel);
         }
@@ -400,61 +393,6 @@ namespace ParserYoula
         {
             Parser parser = new Parser();
             parser.Run().Wait();
-
-
-
-
-            //Save();
-
-            //string fileName = @"result.xlsx";
-            //using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            //{
-            //    using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
-            //    {
-            //        WorkbookPart workbookPart = doc.WorkbookPart;
-            //        SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
-            //        SharedStringTable sst = sstpart.SharedStringTable;
-            //        WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-            //        Worksheet sheet = worksheetPart.Worksheet;
-            //        var cells = sheet.Descendants<Cell>();
-            //        var rows = sheet.Descendants<Row>();
-            //        Console.WriteLine("Row count = {0}", rows.LongCount());
-            //        Console.WriteLine("Cell count = {0}", cells.LongCount());
-            //        // One way: go through each cell in the sheet
-            //        foreach (Cell cell in cells)
-            //        {
-            //            if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
-            //            {
-            //                int ssid = int.Parse(cell.CellValue.Text);
-            //                string str = sst.ChildElements[ssid].InnerText;
-            //                Console.WriteLine("Shared string {0}: {1}", ssid, str);
-            //            }
-            //            else if (cell.CellValue != null)
-            //            {
-            //                Console.WriteLine("Cell contents: {0}", cell.CellValue.Text);
-            //            }
-            //        }
-            //        // Or... via each row
-            //        foreach (Row row in rows)
-            //        {
-            //            foreach (Cell c in row.Elements<Cell>())
-            //            {
-            //                if ((c.DataType != null) && (c.DataType == CellValues.SharedString))
-            //                {
-            //                    int ssid = int.Parse(c.CellValue.Text);
-            //                    string str = sst.ChildElements[ssid].InnerText;
-            //                    Console.WriteLine("Shared string {0}: {1}", ssid, str);
-            //                }
-            //                else if (c.CellValue != null)
-            //                {
-            //                    Console.WriteLine("Cell contents: {0}", c.CellValue.Text);
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
         }
-
-
     }
 }
