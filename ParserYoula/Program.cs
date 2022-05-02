c0f25e3d8019e5d18e80f67e5eb14cf899c28ff4
clackgot <clackgot@gmail.com>
2022-05-03 01:19:54 +0300

    Добавлена выгрузка в excel

--- ParserYoula/Program.cs
+++ ParserYoula/Program.cs
@@ -1,6 +1,11 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -55,13 +60,60 @@ namespace ParserYoula
 
 
             SearchAttributes result = await ParseParamsFromLink(link);
-
+            List<Product> products = new List<Product>();
             await foreach (var product in GetAllProducts(result))
             {
-                Console.WriteLine(product);
+                products.Add(product);
+            }
+            Save(products);
+        }
+
+
+        private static void Save(List<Product> products)
+        {
+            var package = new ExcelPackage();
+
+            var sheet = package.Workbook.Worksheets.Add("Result");
+            sheet.Cells[1, 1].Value = "ID объявление";
+            sheet.Cells[1, 2].Value = "ID владельца";
+            sheet.Cells[1, 3].Value = "Описание";
+            sheet.Cells[1, 4].Value = "Отзывов";
+            sheet.Cells[1, 5].Value = "Название";
+            sheet.Cells[1, 6].Value = "Цена";
+            int row = 2;
+            int col = 1;
+
+            //List<Product> products = new List<Product>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    products.Add(new Product()
+            //    {
+            //        Id = $"Id{i}",
+            //        OwnerId = $"OwnerId{i}",
+            //        Description = $"Description{i}",
+            //        MarksCount = i,
+            //        Name = $"Name{i}",
+            //        Price = i * 100
+            //    });
+            //}
+
+
+            foreach (var product in products)
+            {
+                sheet.Cells[row, col].Value = product.Id;
+                sheet.Cells[row, col + 1].Value = product.OwnerId;
+                sheet.Cells[row, col + 2].Value = product.Description;
+                sheet.Cells[row, col + 3].Value = product.MarksCount;
+                sheet.Cells[row, col + 4].Value = product.Name;
+                sheet.Cells[row, col + 5].Value = product.Price;
+                row++;
             }
 
+            sheet.Protection.IsProtected = true;
+            var excel = package.GetAsByteArray();
+            File.WriteAllBytes("result.xlsx", excel);
         }
+
 
         public async IAsyncEnumerable<Product> GetAllProducts(SearchAttributes searchAttributes)
         {
@@ -348,6 +400,61 @@ namespace ParserYoula
         {
             Parser parser = new Parser();
             parser.Run().Wait();
+
+
+
+
+            //Save();
+
+            //string fileName = @"result.xlsx";
+            //using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            //{
+            //    using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
+            //    {
+            //        WorkbookPart workbookPart = doc.WorkbookPart;
+            //        SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+            //        SharedStringTable sst = sstpart.SharedStringTable;
+            //        WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+            //        Worksheet sheet = worksheetPart.Worksheet;
+            //        var cells = sheet.Descendants<Cell>();
+            //        var rows = sheet.Descendants<Row>();
+            //        Console.WriteLine("Row count = {0}", rows.LongCount());
+            //        Console.WriteLine("Cell count = {0}", cells.LongCount());
+            //        // One way: go through each cell in the sheet
+            //        foreach (Cell cell in cells)
+            //        {
+            //            if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
+            //            {
+            //                int ssid = int.Parse(cell.CellValue.Text);
+            //                string str = sst.ChildElements[ssid].InnerText;
+            //                Console.WriteLine("Shared string {0}: {1}", ssid, str);
+            //            }
+            //            else if (cell.CellValue != null)
+            //            {
+            //                Console.WriteLine("Cell contents: {0}", cell.CellValue.Text);
+            //            }
+            //        }
+            //        // Or... via each row
+            //        foreach (Row row in rows)
+            //        {
+            //            foreach (Cell c in row.Elements<Cell>())
+            //            {
+            //                if ((c.DataType != null) && (c.DataType == CellValues.SharedString))
+            //                {
+            //                    int ssid = int.Parse(c.CellValue.Text);
+            //                    string str = sst.ChildElements[ssid].InnerText;
+            //                    Console.WriteLine("Shared string {0}: {1}", ssid, str);
+            //                }
+            //                else if (c.CellValue != null)
+            //                {
+            //                    Console.WriteLine("Cell contents: {0}", c.CellValue.Text);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
         }
+
+
     }
 }
