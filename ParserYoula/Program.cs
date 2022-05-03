46328779446618cce72b654e74c089ec2271013b
clackgot <clackgot@gmail.com>
2022-05-03 03:58:46 +0300

    Добавлена база данных

--- ParserYoula/Program.cs
+++ ParserYoula/Program.cs
@@ -1,11 +1,11 @@
-﻿using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -65,7 +65,7 @@ namespace ParserYoula
             {
                 products.Add(product);
             }
-            if(!string.IsNullOrEmpty(result.subcategorySlug))
+            if (!string.IsNullOrEmpty(result.subcategorySlug))
             {
                 Save(products, result.subcategorySlug);
             }
@@ -157,7 +157,7 @@ namespace ParserYoula
             {
                 product.OwnerId = jsonResponse["data"]["owner"]["id"].ToString();
                 int marksCount;
-                if(int.TryParse(jsonResponse["data"]["owner"]["rating_mark_cnt"].ToString(), out marksCount))
+                if (int.TryParse(jsonResponse["data"]["owner"]["rating_mark_cnt"].ToString(), out marksCount))
                 {
                     product.MarksCount = marksCount;
                 }
@@ -387,12 +387,157 @@ namespace ParserYoula
             }
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    class YoulaDataBase
+    {
+        private string dbFileName;
+
+        public string DbFileName
+        {
+            get { return dbFileName; }
+        }
+        private SQLiteConnection connection;
+
+        public SQLiteConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private SQLiteCommand command;
+
+        public SQLiteCommand Command
+        {
+            get { return command; }
+        }
+
+        public YoulaDataBase(string name)
+        {
+            connection = new SQLiteConnection();
+            command = new SQLiteCommand();
+            dbFileName = name;
+        }
+
+        public void Create()
+        {
+            bool newDB = false;
+            if (!File.Exists(dbFileName))
+            {
+                SQLiteConnection.CreateFile(dbFileName);
+                newDB = true;
+            }
+
+            try
+            {
+                connection = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                connection.Open();
+                command.Connection = connection;
+
+                if (newDB)
+                {
+                    command.CommandText = @"CREATE TABLE products (id	INTEGER NOT NULL UNIQUE, productId	TEXT NOT NULL UNIQUE,ownerId	TEXT NOT NULL UNIQUE,description	TEXT,price	INTEGER,marks	INTEGER,PRIMARY KEY(id AUTOINCREMENT))";
+                    command.ExecuteNonQuery();
+                    AddTestData();
+                }
+
+                Console.WriteLine("Connected");
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Disconnected");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void AddProductsData()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            command.CommandText = "INSERT INTO products (productId, ownerId, description, price, marks) VALUES (:productId, :ownerId, :description, :price, :marks)";
+            SQLiteTransaction transaction = connection.BeginTransaction();//запускаем транзакцию
+            try
+            {
+                for (int i = 1; i < 101; i++)
+                {
+                    command.Parameters.AddWithValue("productId", $"product_{i}");
+                    command.Parameters.AddWithValue("ownerId", $"owner_{i}");
+                    command.Parameters.AddWithValue("description", $"description_{i}");
+                    command.Parameters.AddWithValue("price", i+2);
+                    command.Parameters.AddWithValue("marks", i*2);
+                    command.ExecuteNonQuery();
+                }
+                transaction.Commit();
+                sw.Stop();
+                Console.WriteLine($"products заполнен за {sw.Elapsed}");
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+
+        private void AddTestData()
+        {
+            AddProductsData();
+        }
+
+        public void AddProducts(List<Product> products)
+        {
+
+        }
+
+
+        public void PrintProducts()
+        {
+            command.CommandText = "SELECT * FROM products";
+            DataTable data = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+            adapter.Fill(data);
+            Console.WriteLine($"Прочитано {data.Rows.Count} записей из таблицы products");
+            foreach (DataRow row in data.Rows)
+            {
+                Console.WriteLine($"id = {row.Field<long>("id")} " +
+                    $"productId = {row.Field<string>("productId")} " +
+                    $"ownerId = {row.Field<string>("ownerId")} " +
+                    $"description = {row.Field<string>("description")} " +
+                    $"price = {row.Field<long>("price")} " +
+                    $"marks = {row.Field<long>("marks")} ");
+            }
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Parser parser = new Parser();
-            parser.Run().Wait();
+            //Parser parser = new Parser();
+            //parser.Run().Wait();
+
+            //if (Connect("firstBase.db"))
+            //{
+            //    Console.WriteLine("Connected");
+            //}
+
+
+            YoulaDataBase dataBase = new YoulaDataBase("profiles.db");
+            dataBase.Create();
+            dataBase.PrintProducts();
+
+
         }
     }
 }
