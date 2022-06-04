08300bd5a34e17acde8a261d175afb7241c20137
clackgot <clackgot@gmail.com>
2022-06-04 18:00:57 +0300

    * Добавлен вывод результата в excel

--- ParserYoula.Data/ExcelContext.cs
+++ ParserYoula.Data/ExcelContext.cs
@@ -20,9 +20,17 @@ namespace ParserYoula.Data
             //sheet.Cells["B1"].Value = "Название";
             //sheet.Cells["C1"].Value = "Описание";
 
-            sheet.Cells[1, 1].Value = "qwe";
+            sheet.Cells[1, 1].Value = "Ссылка";
+            sheet.Cells[1, 2].Value = "Название";
+            sheet.Cells[1, 3].Value = "Описание";
 
-
+            for (int i = 0; i < products.Count(); i++)
+            {
+                sheet.Cells[2 + i, 1].Value = products.ElementAt(i)?.ShortLinkYoula;
+                sheet.Cells[2 + i, 2].Value = products.ElementAt(i)?.Name;
+                sheet.Cells[2 + i, 3].Value = products.ElementAt(i)?.Description;
+            }
+            
             return package.GetAsByteArray();
         }
 
